32f31d4c0ba7cce4f6c66999be8491055d671c75
Francois Lebel <francoislebel@gmail.com>
2015-03-03 08:10:04 -0800

    Cleaned up original project's syntax.

--- retweet.py
+++ retweet.py
@@ -10,22 +10,22 @@ config = ConfigParser.SafeConfigParser()
 config.read(os.path.join(path, 'config'))
 
 # your hashtag or search query and tweet language (empty = all languages)
-hashtag = config.get('settings','search_query')
-tweetLanguage = config.get('settings','tweet_language')
+hashtag = config.get('settings', 'search_query')
+tweet_language = config.get('settings', 'tweet_language')
 
 # blacklisted users and words
-userBlacklist = []
-wordBlacklist = ['RT', u'♺']
+user_blacklist = []
+word_blacklist = ['RT', u'♺']
 
 # build savepoint path + file
-hashedHashtag = hashlib.md5(hashtag).hexdigest()
-last_id_filename = 'last_id_hashtag_%s' % hashedHashtag
+hashed_hashtag = hashlib.md5(hashtag).hexdigest()
+last_id_filename = 'last_id_hashtag_%s' % hashed_hashtag
 rt_bot_path = os.path.dirname(os.path.abspath(__file__))
 last_id_file = os.path.join(rt_bot_path, last_id_filename)
 
 # create bot
-auth = tweepy.OAuthHandler(config.get('twitter','consumer_key'), config.get('twitter','consumer_secret'))
-auth.set_access_token(config.get('twitter','access_token'), config.get('twitter','access_token_secret'))
+auth = tweepy.OAuthHandler(config.get('twitter', 'consumer_key'), config.get('twitter', 'consumer_secret'))
+auth.set_access_token(config.get('twitter', 'access_token'), config.get('twitter', 'access_token_secret'))
 api = tweepy.API(auth)
 
 # retrieve last savepoint if available
@@ -37,7 +37,7 @@ except IOError:
     print 'No savepoint found. Trying to get as many results as possible.'
 
 # search query
-timelineIterator = tweepy.Cursor(api.search, q=hashtag, since_id=savepoint, lang=tweetLanguage).items()
+timelineIterator = tweepy.Cursor(api.search, q=hashtag, since_id=savepoint, lang=tweet_language).items()
 
 # put everything into a list to be able to sort/filter
 timeline = []
@@ -51,8 +51,8 @@ except IndexError:
 
 # filter @replies/blacklisted words & users out and reverse timeline
 timeline = filter(lambda status: status.text[0] != '@', timeline)
-timeline = filter(lambda status: not any(word in status.text.split() for word in wordBlacklist), timeline)
-timeline = filter(lambda status: status.author.screen_name not in userBlacklist, timeline)
+timeline = filter(lambda status: not any(word in status.text.split() for word in word_blacklist), timeline)
+timeline = filter(lambda status: status.author.screen_name not in user_blacklist, timeline)
 timeline.reverse()
 
 tw_counter = 0
@@ -71,7 +71,6 @@ for status in timeline:
     except tweepy.error.TweepError as e:
         # just in case tweet got deleted in the meantime or already retweeted
         err_counter += 1
-        #print e
         continue
 
 print 'Finished. %d Tweets retweeted, %d errors occured.' % (tw_counter, err_counter)
