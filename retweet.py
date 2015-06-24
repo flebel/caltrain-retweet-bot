a63ddaeeeff0ca3fd409593ade6f07b8481cfa34
Francois Lebel <francoislebel@gmail.com>
2015-06-23 22:20:54 -0700

    Cleaned up script.

--- retweet.py
+++ retweet.py
@@ -7,49 +7,56 @@ import os
 
 import tweepy
 
+from collections import defaultdict, namedtuple
+
+
+COUNT_TYPES = namedtuple('COUNT_TYPES', ('errors', 'retweeted',))
 
 parser = argparse.ArgumentParser(description="Retweet a user's tweets matching search terms.")
 parser.add_argument('--config', default='default.conf', dest='config', help='Configuration file')
 
-def main(config_file):
+
+def construct_last_id_filename(config_filename):
+    return config_filename + '.last_id'
+
+def load_config(filename):
     config = ConfigParser.SafeConfigParser()
-    config.read(config_file)
+    config.read(filename)
+    return config
 
-    screen_name = config.get('settings', 'screen_name')
+def persist_last_seen_id(last_id_file_path, last_tweet_id):
+    with open(last_id_file_path, 'w') as f:
+        f.write(str(last_tweet_id))
 
-    # Build savepoint file path
-    last_id_filename = 'last_id.' + config_file
+def retrieve_last_seen_id(last_id_file_path):
+    try:
+        with open(last_id_file_path, 'r') as f:
+            return f.read()
+    except IOError:
+        return ''
+    except:
+        raise
+
+def construct_api(access_token, access_token_secret, consumer_key, consumer_secret):
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    return tweepy.API(auth)
+
+def run(api, last_id_filename, number_tweets_to_retrieve, screen_name, search_terms):
     rt_bot_path = os.path.dirname(os.path.abspath(__file__))
-    last_id_file = os.path.join(rt_bot_path, last_id_filename)
-
-    auth = tweepy.OAuthHandler(config.get('twitter', 'consumer_key'), config.get('twitter', 'consumer_secret'))
-    auth.set_access_token(config.get('twitter', 'access_token'), config.get('twitter', 'access_token_secret'))
-    api = tweepy.API(auth)
+    last_id_file_path = os.path.join(rt_bot_path, last_id_filename)
 
     timeline_kwargs = {
-        'count': config.get('settings', 'number_tweets_to_retrieve'),
+        'count': number_tweets_to_retrieve,
         'include_rts': False,
     }
 
-    # Retrieve last savepoint if available
-    try:
-        with open(last_id_file, 'r') as file:
-            savepoint = file.read()
-        timeline_kwargs['since_id'] = savepoint
-    except IOError:
-        savepoint = ''
-        print 'No savepoint found. Trying to get as many results as possible.'
+    last_tweet_id = retrieve_last_seen_id(last_id_file_path)
+    if last_tweet_id:
+        timeline_kwargs['since_id'] = last_tweet_id
 
     statuses = api.user_timeline(screen_name, **timeline_kwargs)
-
-    try:
-        last_tweet_id = statuses[0].id
-    except IndexError:
-        last_tweet_id = savepoint
-
-    search_terms = filter(None, config.get('settings', 'search_terms').split(','))
-
-    counters = {'retweeted': 0, 'errors': 0}
+    counters = defaultdict(int)
 
     for status in statuses:
         if not any([term for term in search_terms if term in status.text]):
@@ -62,19 +69,34 @@ def main(config_file):
                     'name': status.author.screen_name.encode('utf-8'),
                 }
             api.retweet(status.id)
-            counters['retweeted'] += 1
+            counters[COUNT_TYPES.retweeted] += 1
         except (tweepy.error.TweepError,) as e:
             # Just in case tweet got deleted in the meantime or already retweeted
-            counters['errors'] += 1
+            counters[COUNT_TYPES.errors] += 1
             continue
+
+    try:
+        last_tweet_id = statuses[0].id
+    except IndexError:
+        pass
+
+    # Only persist last seen tweet id if it has changed
+    if timeline_kwargs.get('since_id') != last_tweet_id:
+        persist_last_seen_id(last_id_file_path, last_tweet_id)
 
     print '%(retweeted)d retweeted, %(errors)d errors occurred.' % counters
 
-    # Persist savepoint
-    with open(last_id_file, 'w') as file:
-        file.write(str(last_tweet_id))
-
 if __name__ == '__main__':
     args = parser.parse_args()
-    main(args.config)
+    config = load_config(args.config)
+
+    api = construct_api(access_token=config.get('twitter', 'access_token'),
+                        access_token_secret=config.get('twitter', 'access_token_secret'),
+                        consumer_key=config.get('twitter', 'consumer_key'),
+                        consumer_secret=config.get('twitter', 'consumer_secret'))
+    run(api,
+        last_id_filename=construct_last_id_filename(args.config),
+        number_tweets_to_retrieve=config.get('settings', 'number_tweets_to_retrieve'),
+        screen_name=config.get('settings', 'screen_name'),
+        search_terms=filter(None, config.get('settings', 'search_terms').split(',')))
 
